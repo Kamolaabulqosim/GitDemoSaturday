8cc3ebbff76aa766a7c0a37c40089feceebbe4c8
Kamolaabulqosim <170935581+Kamolaabulqosim@users.noreply.github.com>
2024-09-22 18:31:09 +0200

    Update B.java

--- src/week08_git/B.java
+++ src/week08_git/B.java
@@ -3,5 +3,9 @@ package week08_git;
 public class B {
     public static void main(String[] args) {
         System.out.println("B");
+        System.out.println("lets add more details ");
+
+       
+    
     }
 }
